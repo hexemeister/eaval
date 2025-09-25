2ef0ea8bf44d508ae7d961eaaebf6d0eef1647ae
Renato Moraes <hexemeister@gmail.com>
2025-09-25 16:00:15 +0100

    Quarto teste do main.yml

--- resources/js/layouts/Layout.jsx
+++ resources/js/layouts/Layout.jsx
@@ -4,11 +4,13 @@ import { Footer } from '@/layouts/Footer';
 import { CustomBreadcrumb } from '@/layouts/CustomBreadcrumb';
 
 export function Layout({ children }) {
+  
   const breadcrumbItems = [
     { label: 'Home', href: '/' },
     { label: 'Dashboard', href: '/dashboard' },
     { label: 'Relatórios' }, // Página atual (sem href)
   ];
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
--- resources/js/pages/QuemSomos.tsx
+++ resources/js/pages/QuemSomos.tsx
@@ -6,9 +6,18 @@ type QuemSomosProps = {
 };
 
 export default function QuemSomos({ title }: QuemSomosProps) {
+
+  const breadcrumbItems = [
+    { label: 'Home', href: '/' },
+    { label: 'Quem Somos' }, // Página atual (sem href)
+  ];
+
   return (
     <Layout>
       <Head title={title} />
+      <div className="mb-4">
+        <CustomBreadcrumb items={breadcrumbItems} variant="secondary" />
+      </div>
       <h1 className="text-2xl font-bold mb-4">Quem Somos (WIP)</h1>
 
     </Layout>
