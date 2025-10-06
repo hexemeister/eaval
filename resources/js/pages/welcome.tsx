97f2c4ed11880fd477283c38225ac054084e3366
Renato Moraes <hexemeister@gmail.com>
2025-10-06 21:38:11 +0100

    Altera a página inicial

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,16 +1,16 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Layout } from "@/layouts/Layout";
-import { SearchBar } from "@/layouts/SearchBar";
-import { Link } from "@inertiajs/react";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Layout } from '@/layouts/Layout';
+import { SearchBar } from '@/layouts/SearchBar';
+import { Link } from '@inertiajs/react';
 
-import eavalImgUrl from "../../images/eaval.png";
+import eavalImgUrl from '../../images/eaval.png';
 
 export default function Welcome() {
   return (
     <Layout>
-      <div className="mb-10">
-        <Card>
+      <div className="mb-2">
+        <Card className='-my-4 text-xs'>
           <CardHeader>
             <CardTitle>Aviso:</CardTitle>
           </CardHeader>
@@ -26,6 +26,11 @@ export default function Welcome() {
           </CardContent>
         </Card>
       </div>
+
+      <div className="mx-auto my-4 w-1/4 align-middle">
+        <img src={eavalImgUrl} className="my-10 w-full object-fill" />
+      </div>
+
       <div>
         <TooltipProvider>
           <Tooltip>
@@ -36,14 +41,12 @@ export default function Welcome() {
             </TooltipTrigger>
             <TooltipContent>
               <p className="text-sm">
-                Recursos de pesquisa: use wildcards (*, ?) (ex.: tec*), textos exatos com aspas ("exato"), e operadores booleanos (AND, OR, NOT) (ex.: (educação OR tecnologia) NOT jogo).
+                Recursos de pesquisa: use wildcards (*, ?) (ex.: tec*), textos exatos com aspas ("exato"), e operadores booleanos (AND, OR, NOT) (ex.:
+                (educação OR tecnologia) NOT jogo).
               </p>
             </TooltipContent>
           </Tooltip>
         </TooltipProvider>
-      </div>
-      <div>
-        <img src={eavalImgUrl} className="my-10 w-full" />
       </div>
     </Layout>
   );
