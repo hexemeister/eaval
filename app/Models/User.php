391634bbc516b62f6612ae70582a83da39af8bf2
Renato Moraes <hexemeister@gmail.com>
2025-10-09 14:50:16 +0100

    Corrige bug de login

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,31 +1,16 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Models;
 
-use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-/**
- * Class User
- *
- * @property int $id
- * @property string $name
- * @property string $email
- * @property Carbon|null $email_verified_at
- * @property string $password
- * @property string|null $remember_token
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- */
-class User extends Model
+class User extends Authenticatable
 {
-    protected $table = 'users';
-
-    protected $primaryKey = 'id';
-
-    public $timestamps = true;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -35,37 +20,29 @@ class User extends Model
     protected $fillable = [
         'name',
         'email',
-        'email_verified_at',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
         'password',
         'remember_token',
     ];
 
     /**
-     * The model's default values for attributes.
+     * Get the attributes that should be cast.
      *
-     * @var array<string, mixed>
-     */
-    protected $attributes = [
-    ];
-
-    protected $hidden = [
-        'password',
-    ];
-
-    /**
      * @return array<string, string>
      */
     protected function casts(): array
     {
         return [
-            'id' => 'integer',
-            'name' => 'string',
-            'email' => 'string',
             'email_verified_at' => 'datetime',
-            'password' => 'string',
-            'remember_token' => 'string',
-            'created_at' => 'datetime',
-            'updated_at' => 'datetime',
+            'password' => 'hashed',
         ];
     }
 }
