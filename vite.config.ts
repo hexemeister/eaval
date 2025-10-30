236fed8d71d578b05998d8fd42558ee832bd7132
Renato Moraes <hexemeister@gmail.com>
2025-10-30 20:10:43 +0000

    tentativa 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
@@ -19,5 +20,10 @@ export default defineConfig({
     ],
     esbuild: {
         jsx: 'automatic',
+    },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'resources/js'),
+        },
     },
 });
